a4a5078b913ecc1884a2b74ef236c4e399807072
Leander Merbecks <leander.merbecks@gmx.de>
2023-05-29 01:50:13 +0200

    Seasonal analysis

--- src/main.py
+++ src/main.py
@@ -42,7 +42,41 @@ def plotMeanHourly(meanHourlyValue, datatitle, ylabel, yearLimits, location):
     plt.xlabel('Stunde am Tag')
     fig.show()
 
+def plotMeanHourlySeasons(meanHourlySeasonsValues: pd.DataFrame, datatitle: str, ylabel: str):
+    """Plots mean hourly seasonal data in four subplots. One for each season
 
+    Args:
+        meanHourlySeasonsValues (pd.DataFrame): Seasonal data
+        datatitle (str): data title
+        ylabel (str): label for y axes of subplots
+    """
+
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    seasonsString = ['Winter (Dez - Feb)', 'Frühling (Mär - Mai)', 'Sommer (Jun - Aug)', 'Herbst (Sep - Nov)']
+    for season in range(1,5):
+        curraxes = axes[(season-1)//2,1-season%2]
+        currDf = meanHourlySeasonsValues.loc[meanHourlySeasonsValues['Season'] == season]
+        curraxes.bar(range(currDf['Hour'].min(),currDf['Hour'].max() + 1), currDf['Wert'])
+        curraxes.set_title(seasonsString[season-1])
+        curraxes.set_ylabel(ylabel)
+    fig.suptitle(datatitle)
+    fig.show()
+
+
+
+def calcMeanHourlySeasons(df: pd.DataFrame, valueColumn: str):
+    """Calculates the mean hourly value for the four seasons winter spring summer fall (ID: 1,2,3,4)
+
+    Args:
+        df (pandas Dataframe)): Dataframe with value and timestamp
+        valueColumn (string): Name of value column
+
+    Returns:
+        pandas Dataframe: Dataframe with mean daily values by seasons
+    """
+    df['Month'] = df['TimeStamp'].dt.month
+    df['Season'] = df['Month'] % 12 // 3 + 1 #seasons winter spring summer fall (1,2,3,4)
+    return df.groupby(['Season', 'Hour'])[valueColumn].mean().to_frame().reset_index()
 
 
 
@@ -101,11 +135,11 @@ reassignTimestamp(df=sunDurationDf, timeStampHeader='Zeitstempel')
 
 meanSunDurationYear = calcMeanDaily(df=sunDurationDf, valueColumn='Wert')
 meanSunDurationDay = calcMeanHourly(df=sunDurationDf, valueColumn='Wert')
+meanSunDurationDaySeasons = calcMeanHourlySeasons(df=sunDurationDf, valueColumn='Wert')
 
-
-plotMeanDaily(meanDailyValue=meanSunDurationYear, datatitle='Mittlere tägliche Sonnenscheindauer', ylabel='Sonnenscheindauer [h]', minYear=sunDurationDf['Year'].min(), maxYear=sunDurationDf['Year'].max(), location='Essen')
-plotMeanHourly(meanHourlyValue=meanSunDurationDay, datatitle='Mittlere stündliche Sonnenscheindauer', ylabel='Sonnenscheindauer [h]', yearLimits=[sunDurationDf['Year'].min(), sunDurationDf['Year'].max()], location='Essen')
-
+plotMeanDaily(meanDailyValue=meanSunDurationYear, datatitle='Mittlere tägliche Sonnenscheindauer', ylabel='Sonnenscheindauer [min]', minYear=sunDurationDf['Year'].min(), maxYear=sunDurationDf['Year'].max(), location='Essen')
+plotMeanHourly(meanHourlyValue=meanSunDurationDay, datatitle='Mittlere stündliche Sonnenscheindauer', ylabel='Sonnenscheindauer [min]', yearLimits=[sunDurationDf['Year'].min(), sunDurationDf['Year'].max()], location='Essen')
+plotMeanHourlySeasons(meanHourlySeasonsValues= meanSunDurationDaySeasons, datatitle='Mittlere stündliche Sonnenscheindauer nach Jahreszeiten', ylabel='Sonnenscheindauer [min]')
 
 # Overcast
 overcastDf = pd.read_csv(r"C:\Users\lelem\Projekte\WetterDatenanalyse\WeatherDataAnalysisESS\Data\cdc_download_2023-05-17_14-28\data\data_OBS_DEU_PT1H_RR.csv", index_col=False)
@@ -113,10 +147,12 @@ reassignTimestamp(df=overcastDf, timeStampHeader='Zeitstempel')
 
 meanOvercastYear = calcMeanDaily(df=overcastDf, valueColumn='Wert')
 meanOvercastDay = calcMeanHourly(df=overcastDf, valueColumn='Wert')
+meanOvercastDaySeasons = calcMeanHourlySeasons(df=overcastDf, valueColumn='Wert')
 
 
-plotMeanDaily(meanDailyValue=meanOvercastYear, datatitle='Mittlerer täglicher Bedeckungsgrad', ylabel='Bedeckungsgrad [Quart]', minYear=overcastDf['Year'].min(), maxYear=overcastDf['Year'].max(), location='Essen')
-plotMeanHourly(meanHourlyValue=meanOvercastDay, datatitle='Mittlerer stündlicher Bedeckungsgrad', ylabel='Bedeckungsgrad [Quart]', yearLimits=[overcastDf['Year'].min(), overcastDf['Year'].max()], location='Essen')
+plotMeanDaily(meanDailyValue=meanOvercastYear, datatitle='Mittlerer täglicher Bedeckungsgrad', ylabel='Bedeckungsgrad [Okta]', minYear=overcastDf['Year'].min(), maxYear=overcastDf['Year'].max(), location='Essen')
+plotMeanHourly(meanHourlyValue=meanOvercastDay, datatitle='Mittlerer stündlicher Bedeckungsgrad', ylabel='Bedeckungsgrad [Okta]', yearLimits=[overcastDf['Year'].min(), overcastDf['Year'].max()], location='Essen')
+plotMeanHourlySeasons(meanHourlySeasonsValues=meanOvercastDaySeasons, datatitle='Mittlerer stündlicher Bedeckungsgrad nach Jahreszeiten', ylabel='Bedeckungsgrad [Okta]')
 
 # Precipitation
 precipitationDf = pd.read_csv(r"C:\Users\lelem\Projekte\WetterDatenanalyse\WeatherDataAnalysisESS\Data\cdc_download_2023-05-17_14-28\data\data_OBS_DEU_PT1H_N.csv", index_col=False)
@@ -124,8 +160,10 @@ reassignTimestamp(df=precipitationDf, timeStampHeader='Zeitstempel')
 
 meanPrecipitationYear = calcMeanDaily(df=precipitationDf, valueColumn='Wert')
 meanPrecipitationDay = calcMeanHourly(df=precipitationDf, valueColumn='Wert')
+meanPrecipitationDaySeasons = calcMeanHourlySeasons(df=precipitationDf, valueColumn='Wert')
 
 
 plotMeanDaily(meanDailyValue=meanPrecipitationYear, datatitle='Mittlerer täglicher Niederschlag', ylabel='Niederschlag [mm]', minYear=precipitationDf['Year'].min(), maxYear=precipitationDf['Year'].max(), location='Essen')
 plotMeanHourly(meanHourlyValue=meanPrecipitationDay, datatitle='Mittlerer stündlicher Niederschlag', ylabel='Niederschlag [mm]', yearLimits=[precipitationDf['Year'].min(), precipitationDf['Year'].max()], location='Essen')
+plotMeanHourlySeasons(meanHourlySeasonsValues=meanPrecipitationDaySeasons, datatitle='Mittlerer stündlicher Niederschlag nach Jahreszeiten', ylabel='Niederschlag [mm]')
 plt.show()
